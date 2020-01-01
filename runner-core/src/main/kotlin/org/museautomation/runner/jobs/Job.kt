27469ef49e68855f5ce07f2ba8887966a5fe32a6
ChrisLMerrill <chris@ide4selenium.com>
2020-01-01 09:09:10 -0500

    [core] FEATURE - add inputs[] to Job

--- runner-core/src/main/kotlin/org/museautomation/runner/jobs/Job.kt
+++ runner-core/src/main/kotlin/org/museautomation/runner/jobs/Job.kt
@@ -1,8 +1,11 @@
 package org.museautomation.runner.jobs
 
+import org.musetest.core.values.descriptor.SubsourceDescriptor
+
 /**
  * @author Christopher L Merrill (see LICENSE.txt for license details)
  */
 data class Job (var id: String,
                 var taskId: String,
-                var projectId: String)
+                var projectId: String,
+                var inputs: List<SubsourceDescriptor>)
