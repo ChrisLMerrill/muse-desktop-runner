8acb30e0e7ca93753bc1314f9d677f05cd414a58
ChrisLMerrill <chris@ide4selenium.com>
2020-01-01 09:08:50 -0500

    [desktop] FEATURE - add basic UI tabs

--- desktop/src/main/kotlin/org/museautomation/runner/desktop/MainWindow.kt
+++ desktop/src/main/kotlin/org/museautomation/runner/desktop/MainWindow.kt
@@ -3,9 +3,14 @@ package org.museautomation.runner.desktop
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.Tab
+import javafx.scene.control.TabPane
 import javafx.scene.layout.GridPane
 import javafx.stage.Stage
 import org.museautomation.runner.desktop.RunnerDesktopApp.Companion.APP
+import org.museautomation.runner.jobs.JobRuns
+import org.museautomation.runner.jobs.Jobs
 import org.museautomation.runner.projects.RegisteredProjects
 
 class MainWindow : Application()
@@ -14,19 +19,21 @@ class MainWindow : Application()
     {
         APP.main_stage = stage
 
-        val hello_button = Button()
-        hello_button.text = "Print Greeting"
-        hello_button.setOnAction { println("Hello, World!") }
+        val tabs = TabPane()
 
-        val exit_button = Button()
-        exit_button.text = "Exit"
-        exit_button.setOnAction { APP.shutdown() }
+        val projects_tab = Tab("Projects")
+        projects_tab.content = Label("There are ${RegisteredProjects.asList().size} projects")
+        tabs.tabs.add(projects_tab)
 
-        val root_pane = GridPane()
-        root_pane.children.add(hello_button)
-        root_pane.children.add(exit_button)
+        val jobs_tab = Tab("Jobs")
+        jobs_tab.content = Label("There are ${Jobs.asList().size} jobs")
+        tabs.tabs.add(jobs_tab)
 
-        val scene = Scene(root_pane, 300.0, 250.0)
+        val runs_tab = Tab("Runs")
+        runs_tab.content = Label("There are ${JobRuns.asList().size} runs")
+        tabs.tabs.add(runs_tab)
+
+        val scene = Scene(tabs, 300.0, 250.0)
 
         stage.setOnCloseRequest(
         { event ->
@@ -34,7 +41,7 @@ class MainWindow : Application()
             APP.main_stage = null
             event.consume()
         })
-        stage.title = "Hello World app"
+        stage.title = "Muse Runner"
         stage.scene = scene
         stage.show()
 
